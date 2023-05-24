63bf4003993badea265d2e35a950f32a2d37d7a1
Olexie <116206464+Olexie@users.noreply.github.com>
2023-05-24 20:48:48 +0100

    fixup test on citysearch

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
-  constructor() {
-    super();
-    this.state = {
-      query: 'Munich',
-    };
-    render();
-    {
-       return (
+  state = {
+    query: '',
+  };
+  render() {
+    return (
       <div className="CitySearch">
         value={this.state.query}
         <input type="text" className="city" value={this.state.query} />
         <ul className="suggestions"></ul>
-      </div>;
-      // );
-    }
+      </div>
+    );
   }
 }
 
